c5a0f24aba042b7a196ed80f75672c32446292b5
AmnNdm <95464163+AmnNdm@users.noreply.github.com>
2022-02-09 11:50:47 +0500

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,5 +3,6 @@ class Constants {
   final String posturl = "https://api.imgur.com/3/upload";
   final String baseurl = "https://i.imgur.com/";
   final String ext = ".jpeg";
-  final String clientID = "Client-ID 00dc3cf03fd15c9";
+  final String id = "your_client_id";
+  final String clientID = "Client-ID $id";
 }
